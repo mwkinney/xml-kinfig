314f328d971a5e94a62c6f622e2759b28a42e54b
yenic <mwkinney@gmail.com>
2014-04-18 00:21:12 -0500

    Stopping point for new VM

--- main.py
+++ main.py
@@ -1,71 +1,71 @@
-import time
-import glob
-from os.path import expanduser
+from time import time
+from glob import glob
+from tempfile import NamedTemporaryFile as namedtemporaryfile
+from os import remove
+from shutil import copy
 from sys import exit
-import re
-#from tempfile import TemporaryFile
-import tempfile
-import shutil
-#import os
+from re import sub
+from os.path import expanduser #for test environment only
+from os import system #for test environment only
 
-class XmlHandler(object):
-    def __init__(self, rootdir):
-        start = time.time()
-        #make/reset list
-        dir_list = []
-        #make a list of files (and folders)
-        dir_list += glob.glob(rootdir + '/*/???????????????/???????/???.??????')
-        dir_list += glob.glob(rootdir + '/*/???????????????/???.??????')
-
-    def modify_files(self, old_string, new_string):
-        for d in dir_list: # for-each file
-            originalFile = open(d, 'r') # open file and read it
-            tempFile = tempfile.NamedTemporaryFile('w+b', delete=False) # open a tempfile for read/write 
-            shutil.copy (originalFile, tempFile.name) #copy originalFile over tempFile
-            for line in tempfile: #replace strings in tempfile
-                tempfile.write(re.sub(line.replace(old_string, new_string flags=re.IGNORECASE | re.MULTILINE)))
-            #close the tempfile
-            #rename tempfile over original file's name
-            #deleteme- shutil.copy (originalFile, tempFile) #copy tempFile over originalFile 
-            #deleteme- originalFile.close() #save?? close originalFile
-            #deleteme- tempFile.close() #close tempfile (deletes it, also GC'd by default)
-        end = time.time()
-        print ('All done\n', len(dir_list),'files updated\n', \
-        '{0:.2f}'.format(end - start), 'seconds elapsed')
-
-class UserInput(object):
+class Engine(object):
     def __init__(self):
-		#server_string = input("Enter servername eg. 'h1-chdevws13' without quotes\n> ")
-        server = input("Enter servername eg. 'test' without quotes\n> ")
-        #rootdir = r'//' + server_string + '/www'
-        rootdir = expanduser('~/') + server
-        #new_string = input("Enter SMTP value eg. 'internal.elitecorp.com' without "
-        #"quotes\n> ")
-        old_string = input("Enter original text value eg.\
-        'internal.hubbardone.net' without quotes. This is not case\
-        sensitive.\n> ")
-        new_string = input("Enter SMTP value eg. 'localhost' without\
-                quotes. This is not case sensitive.\n> ")
+        system('clear') #for test environment only
+        print ("'xml-kinfig'\nConception, design and programming by yenic\nUse at your own peril, no warranty or"\
+        " support provided\n")
+		#server = input("Enter servername eg. 'h1-chdevws13' without quotes\n> ")
+        self.server = raw_input("Enter servername eg. 'test' without quotes\n> ") #for test environment only
+        #rootdir = r'//' + server + '/www'
+        self.rootdir = expanduser('~/') + self.server #for test environment only
+        self.old_string = raw_input("Enter original text value eg."\
+        "'internal.hubbardone.net' without quotes. This is not case"\
+        " sensitive.\n> ") 
+        #new_string = input("Enter new text value eg. 'internal.elitecorp.com' without "
+        #"quotes. This is not case sensitive.\n> ")
+        self.new_string = raw_input("Enter new text value eg. 'LocalHostTest' without"\
+                " quotes. This is not case sensitive.\n> ") #for test environment only
 
-    def accept_parameters(self):
-        print ('xml-kinfig\nby Mark Kinney\nUse at your own peril, no warranty or'\
-        ' support provided\n')
+    def modify_files(self, old_string, new_string, dir_list):
+        start = time() #begin counter
+        for d in dir_list: # for-each file
+            self.originalfile = open(d, 'r') # open file and read it
+            self.originalfile.close()
+            self.tempfile = namedtemporaryfile('w+b', delete=False) # open a tempfile for read/write 
+            copy(self.originalfile, self.tempfile.name) #copy originalFile over tempFile
+            for line in self.tempfile: #replace strings in tempfile
+                self.tempfile.write(re.sub (line.replace (self.old_string, self.new_string, flags=re.IGNORECASE | re.MULTILINE)))
+            self.tempfile.close() #close the tempfile
+            shutil.copy(self.tempfile.name, self.originalfile) #rename (move) tempfile over original file's name
+            os.remove(self.tempfile.name) #delete tempfile
+        end = time()
+        print ("All done\n", len(dir_list),"files updated\n", \
+        "{0:.2f}".format(end - start), "seconds elapsed")
 
+class Validator(object):
+    def __init__(self, startup):
+        self.startup = startup
+        self.dir_list = [] #make/reset list
+        self.dir_list += glob(startup.rootdir + '/*/???????????????/???????/???.??????') #make list of files (and folders)
+        self.dir_list += glob(startup.rootdir + '/*/???????????????/???.??????') #TODO- use os.path.join() instead of appending paths
+
+    def accept_parameters(self, startup):
         while True:
             print ('Run now? Y/N')
             tokenTuple = {
-                'Y':(modify_files, (old_string, new_string)),
+                'Y':(self.startup.modify_files, (self.startup.old_string, self.startup.new_string, self.dir_list)),
                 'N':(exit, ()),
-                }
-            user_input = (input("> "))
-            fn, args = tokenTuple.get(user_input, (None,None))
+                } #pack the tuple
+            self.user_input = (raw_input("> "))
+            fn, args = tokenTuple.get(self.user_input, (None,None)) #unpack the tuple, TODO- better comments here
             if fn == None:
                 continue
             else:
                 fn(*args)
                 break
 
-startup = UserInput()
-xml = XmlHandler()
-startup.accept_parameters()
-xml.modify_files()
+startup = Engine()
+xml = Validator(startup)
+xml.accept_parameters(startup)
+
+#xml.accept_parameters()
+#startup.modify_files()
